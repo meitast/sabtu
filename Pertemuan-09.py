b2fe1b8831149781a1f3e477d0365af0d38e1b84
Meita Indah Fadilla <meitafadilla20@gmail.com>
2023-11-11 15:11:38 +0700

    Menambahkan metric

--- Pertemuan-09.py
+++ Pertemuan-09.py
@@ -1,16 +1,27 @@
 import streamlit as st
 
+# ini bagian heading aplikasi streamlit
 st.title("Pertemuan Praktikum Big Data")
 st.write("Meita Indah Fadilla")
 st.write("# Heading 1")
 st.write("## Heading 2")
 st.write("### Heading 3")
+st.write("#### Semangat") #maksimal 6 hastag
 
+# kinerja unit
+st.metric("Kinerja", 40, -1)
+st.metric("Response Time", 30, 20)
+
+# pilihan
 pilih1 = st.checkbox('Ya')
 pilih2 = st.checkbox('Tidak')
 
 st.write(pilih1)
 st.write(pilih2)
+
+"""
+ini komentar
+"""
 
 makanan = st.radio('Makanan kesukaan',['Bakso', 'Nasi Goreng', 'Mie Ayam'])
 
